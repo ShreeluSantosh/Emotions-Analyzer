ddb2251de59cba02f50587bb55c0dc33d4dccfda
Shreelu Santosh <94289402+ShreeluSantosh@users.noreply.github.com>
2024-01-31 21:16:24 +0530

    Delete streamlit_frontend.py

--- streamlit_frontend.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Web App - GoEmotions")
-
-st.markdown("Credit: Google Research - GoEmotions")
-st.markdown("https://github.com/google-research/google-research/tree/3c7c1ddae388ae79a4c27761b71c187d0e3b5f5e/goemotions")
-
-st.text("This web app takes the input text, and predicts the emotions expressed in the text")
